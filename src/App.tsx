8e4ceac9b9df4e783cf30a5d421402c8747a2d06
Timberzzman <leloupandco@gmail.com>
2022-10-30 00:17:28 +0200

    feat: add files listing from directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,77 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import {createSignal, For} from "solid-js";
+import {invoke} from "@tauri-apps/api/tauri";
+import {BaseDirectory, readDir, FileEntry} from '@tauri-apps/api/fs';
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+    const [greetMsg, setGreetMsg] = createSignal("");
+    const [name, setName] = createSignal("");
+    const [files, setFiles] = createSignal<Array<FileEntry>>([]);
 
-  async function returnString() {
-      const result = await invoke('return_string', {
-          word: 'This is the argument'
-      });
+    async function returnString() {
+        const result = await invoke('return_string', {
+            word: 'This is the argument'
+        });
 
-      alert(result);
-  }
+        alert(result);
+    }
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+    async function greet() {
+        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        setGreetMsg(await invoke("greet", {name: name()}));
+    }
 
-  return (
-    <div class="container">
-      <h1>Welcome to Tauri!</h1>
+    async function listAllFilesInDirectory() {
+        const files = await readDir(name(), {dir: BaseDirectory.Home});
+        setFiles(files);
+    }
 
-      <div class="row">
-          <a href="https://solidjs.com" target="_blank">
-            <img src={logo} class="logo solid" alt="Solid logo" />
-          </a>
-      </div>
+    return (
+        <div class="container">
+            <p>The directory must be in your $HOME directory</p>
+            <div class="row">
+                <div>
+                    <input
+                        id="greet-input"
+                        onChange={(e) => setName(e.currentTarget.value)}
+                        placeholder="Enter a name..."
+                    />
+                    <button type="button" onClick={() => returnString()}>
+                        Try click
+                    </button>
+                    <button type="button" onClick={() => listAllFilesInDirectory()}>
+                        List files
+                    </button>
+                </div>
 
-      <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-          <button type="button" onClick={() => returnString()}>
-            Try click
-          </button>
+                <p>{greetMsg}</p>
+            </div>
+            <div class="row">
+                <table>
+                    <thead>
+                    <tr>
+                        <td>
+                            Filename
+                        </td>
+                        <td>
+                            Path
+                        </td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <For each={files()}>
+                        {(file: FileEntry) => (
+                            <tr>
+                                <td>{file.name}</td>
+                                <td>{file.path}</td>
+                            </tr>
+                        )}
+                    </For>
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+    );
 }
 
 export default App;
